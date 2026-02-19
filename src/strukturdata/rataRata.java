1c09f5bfd9e366bfe5396083348e26a80a190392
yondheni <yondheni@gmail.com>
2026-02-19 21:30:57 +0700

    Update rataRata.java

--- src/strukturdata/rataRata.java
+++ src/strukturdata/rataRata.java
@@ -1,1 +1,16 @@
+public class tambahdanrata {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int total = 0;
+
+        for (int i = 0; i < angka.length; i++) {
+            total += angka[i];
+        }
+
+        double rata = (double) total / angka.length;
+
+        System.out.println("Tambah : " + total);
+        System.out.println("Rata-rata : " + rata);
+    }
+}
 
