d36380d289113bd4d0cfe66edc50bc87fb7e17bf
yohanes dheni <Hype AMD@HYPE>
2026-02-19 21:43:25 +0700

    new update

---
+++ src/strukturdata/jumlah.java
@@ -0,0 +1,10 @@
+public class jumlah {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int total = 0;
+        for (int i = 0; i < angka.length; i++) {
+            total += angka[i];
+        }
+        System.out.println("Tambah : " + total);
+    }
+}
